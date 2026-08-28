1f10f11ec55644d378e888113ee779f6e3e1cc97
Júlia <juulia.p.farias@gmail.com>
2026-08-28 16:51:47 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,36 @@
+
+
+let n1 = Number(prompt("Digite um número:"))
+let n2 = Number(prompt("Digite outro número:"))
+let soma = n1 + n2
+console.log(soma)
+
+
+
+
+
+let n3 = Number(prompt("Digite um número:"))
+let n4 = Number(prompt("Digite outro número:"))
+let subtração = n3 - n4
+console.log(subtração)
+
+
+
+
+
+let n5 = Number(prompt("Digite um número:"))
+let n6 = Number(prompt("Digite outro número:"))
+let multiplicação = n5 * n6
+console.log(multiplicação)
+
+
+
+
+
+let n7 = Number(prompt("Digite um número:"))
+let n8 = Number(prompt("Digite outro número:"))
+let multiplicação = n7/n8
+console.log(divisão)
+
+
+
